d32e5d6825a7808c3ddb2e32ba9261a4530f420b
91934 <91934@LAPTOP-7N07FBDU>
2021-06-11 10:01:17 +0530

    first commit....

---
+++ GithubDemo/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int age=10;
+		 
+		 if(age==10)
+		 {
+			 System.out.println("age is 10");
+		 }
+		 else
+		 {
+			 System.out.println("age is above 10");
+		 }
+
+	}
+
+}
